57016f01a039d6739a49210bed088374e9f31258
MrZoinker <mrzoinker@gmail.com>
2017-03-25 16:51:37 +0000

    ManageProfileWIP2

--- SkreppIsUI/app/src/main/java/com/app/skreppis/skreppis/ManageProfileActivity.java
+++ SkreppIsUI/app/src/main/java/com/app/skreppis/skreppis/ManageProfileActivity.java
@@ -1,5 +1,7 @@
 package com.app.skreppis.skreppis;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
@@ -7,6 +9,8 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.app.skreppis.skreppis.R;
@@ -16,24 +20,192 @@ import static android.R.attr.y;
 
 public class ManageProfileActivity extends BaseActivity {
 
-    ImageView imageView;
+    Button mChangeUnameBt;
+    Button mSaveUnameBt;
+    Button mChangeFnameBt;
+    Button mSaveFnameBt;
+    Button mChangeLnameBt;
+    Button mSaveLnameBt;
+    Button mChangePhoneBt;
+    Button mSavePhoneBt;
+    Button mChangePasswordBt;
+    Button mBecomeDriverBt;
+    Button mDebugDriverBt;
+    Button mChangeSeatsBt;
+    Button mSaveSeatsBt;
+    Button mChangeSmokeBt;
+    Button mSaveSmokeBt;
 
-    Drawable drawable;
-    Bitmap bitmap;
+    View mUnameView;
+    View mUnameCView;
+    View mFnameView;
+    View mFnameCView;
+    View mLnameView;
+    View mLnameCView;
+    View mPhoneView;
+    View mPhoneCView;
+    View mSeatsView;
+    View mSeatsCView;
+    View mSmokeView;
+    View mSmokeCView;
+    View mDriverView;
+    //ImageView imageView;
+    boolean isDriver;
+    //Drawable drawable;
+    //Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        makepref();
+        isDriver = true;
         setContentView(R.layout.activity_manage_profile);
 
-        imageView = (ImageView) findViewById(R.id.profileImage);
 
-        drawable = ContextCompat.getDrawable(this, R.drawable.profile_pic);
-        bitmap = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap newBitmap = convertImage(bitmap);
-        imageView.setImageBitmap(newBitmap);
+        mChangeUnameBt = (Button) findViewById(R.id.btn_man_profile_ch_uname);
+        mSaveUnameBt = (Button) findViewById(R.id.btn_man_profile_sv_uname);
+        mChangeFnameBt = (Button) findViewById(R.id.btn_man_profile_ch_fname);
+        mSaveFnameBt = (Button) findViewById(R.id.btn_man_profile_sv_fname);
+        mChangeLnameBt = (Button) findViewById(R.id.btn_man_profile_ch_lname);
+        mSaveLnameBt = (Button) findViewById(R.id.btn_man_profile_sv_lname);
+        mChangePhoneBt = (Button) findViewById(R.id.btn_man_profile_ch_phone);
+        mSavePhoneBt = (Button) findViewById(R.id.btn_man_profile_sv_phone);
+        mChangeSeatsBt = (Button) findViewById(R.id.btn_man_profile_ch_seats);
+        mSaveSeatsBt = (Button) findViewById(R.id.btn_man_profile_sv_seats);
+        mChangeSmokeBt = (Button) findViewById(R.id.btn_man_profile_ch_smoke);
+        mSaveSmokeBt = (Button) findViewById(R.id.btn_man_profile_sv_smoke);
+        mBecomeDriverBt = (Button) findViewById(R.id.bt_man_profile_become_driver);
+        mDebugDriverBt = (Button) findViewById(R.id.bt_man_profile_debug_driver);
+
+        mUnameView = findViewById(R.id.man_uname_view);
+        mUnameCView = findViewById(R.id.man_uname_cview);
+        mFnameView = findViewById(R.id.man_fname_view);
+        mFnameCView = findViewById(R.id.man_fname_cview);
+        mLnameView = findViewById(R.id.man_lname_view);
+        mLnameCView = findViewById(R.id.man_lname_cview);
+        mPhoneView = findViewById(R.id.man_phone_view);
+        mPhoneCView = findViewById(R.id.man_phone_cview);
+        mSeatsView = findViewById(R.id.man_seats_view);
+        mSeatsCView = findViewById(R.id.man_seats_cview);
+        mSmokeView = findViewById(R.id.man_smoke_view);
+        mSmokeCView = findViewById(R.id.man_smoke_cview);
+        mDriverView = findViewById(R.id.man_driver_view);
+
+        driverMode();
+
+        mChangeUnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mUnameView, mUnameCView);
+            }
+        });
+        mSaveUnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mUnameView, mUnameCView);
+            }
+        });
+        mChangeFnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mFnameView, mFnameCView);
+            }
+        });
+        mSaveFnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mFnameView, mFnameCView);
+            }
+        });
+        mChangeLnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mLnameView, mLnameCView);
+            }
+        });
+        mSaveLnameBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mLnameView, mLnameCView);
+            }
+        });
+        mChangePhoneBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mPhoneView, mPhoneCView);
+            }
+        });
+        mSavePhoneBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mPhoneView, mPhoneCView);
+            }
+        });
+        mChangeSeatsBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mSeatsView, mSeatsCView);
+            }
+        });
+        mSaveSeatsBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mSeatsView, mSeatsCView);
+            }
+        });
+        mChangeSmokeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(true, mSmokeView, mSmokeCView);
+            }
+        });
+        mSaveSmokeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapVisible(false, mSmokeView, mSmokeCView);
+            }
+        });
+        mBecomeDriverBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isDriver = true;
+                driverMode();
+            }
+        });
+        mDebugDriverBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isDriver = false;
+                driverMode();
+            }
+        });
+
+        //imageView = (ImageView) findViewById(R.id.profileImage);
+
+        //drawable = ContextCompat.getDrawable(this, R.drawable.profile_pic);
+        //bitmap = ((BitmapDrawable) drawable).getBitmap();
+        //Bitmap newBitmap = convertImage(bitmap);
+        //imageView.setImageBitmap(newBitmap);
 
     }
 
+    //Swaps visibility of two views.
+    private void swapVisible(boolean show, View v1, View v2){
+        v1.setVisibility(show ? View.GONE : View.VISIBLE);
+        v2.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    private void driverMode() {
+        if(isDriver){
+            mBecomeDriverBt.setVisibility(View.GONE);
+            mDebugDriverBt.setVisibility(View.VISIBLE);
+            mDriverView.setVisibility(View.VISIBLE);
+        }else{
+            mBecomeDriverBt.setVisibility(View.VISIBLE);
+            mDebugDriverBt.setVisibility(View.GONE);
+            mDriverView.setVisibility(View.GONE);
+        }
+    }
+/*
     //For the profile picture to appear in grayscale
     //happy days
     //prob not that efficient tho
@@ -65,6 +237,6 @@ public class ManageProfileActivity extends BaseActivity {
         }
         return finalImage;
     }
-
+*/
 
 }
