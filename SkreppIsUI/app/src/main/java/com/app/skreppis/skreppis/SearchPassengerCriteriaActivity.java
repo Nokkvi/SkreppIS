654ccbee63bf35e0b4cef99e8f5a06617c74854d
MrZoinker <mrzoinker@gmail.com>
2017-03-26 16:37:51 +0000

    Find Passenger Toggle and Search layouts

---
+++ SkreppIsUI/app/src/main/java/com/app/skreppis/skreppis/SearchPassengerCriteriaActivity.java
@@ -0,0 +1,13 @@
+package com.app.skreppis.skreppis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchPassengerCriteriaActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_passenger_criteria);
+    }
+}
